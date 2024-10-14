dfdf49d6fe0edd6c7889a408027f345ae6b79716
Freeagles1991 <dmitry.nerovny@gmail.com>
2024-10-14 19:35:58 +0300

    Добавил дефолтный инициализатор для NFT

--- FakeNFT/Models/Network/Nft.swift
+++ FakeNFT/Models/Network/Nft.swift
@@ -8,4 +8,15 @@ struct Nft: Codable {
     let price: Double
     let author: String
     let id: String
+    
+    init(createdAt: String = "", name: String  = "Default", images: [URL] = [], rating: Int = 5, description: String = "", price: Double = 1.99, author: String = "Default", id: String = "Default") {
+        self.createdAt = createdAt
+        self.name = name
+        self.images = images
+        self.rating = rating
+        self.description = description
+        self.price = price
+        self.author = author
+        self.id = id
+    }
 }
