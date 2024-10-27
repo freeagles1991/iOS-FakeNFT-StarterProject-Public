c970a9219d0bec50eeb7256f65eb1bcbb0a8f408
Arthur <Fokusnik90999@gmail.com>
2024-10-27 16:50:19 +0500

    Добавил сервис для загрузки профиля

---
+++ FakeNFT/Services/Requests/UserProfileRequest.swift
@@ -0,0 +1,21 @@
+//
+//  UserProfileRequest.swift
+//  FakeNFT
+//
+//  Created by Артур  Арсланов on 27.10.2024.
+//
+
+import Foundation
+
+struct UserProfileRequest: NetworkRequest {
+    var dto: Dto?
+    
+    var endpoint: URL? {
+        return URL(string: "\(RequestConstants.baseURL)/api/v1/profile/1")
+    }
+    
+    var httpMethod: HttpMethod {
+        return .get
+    }
+    
+}
---
+++ FakeNFT/Services/UserProfileService.swift
@@ -0,0 +1,38 @@
+//
+//  UserProfileService.swift
+//  FakeNFT
+//
+//  Created by Артур  Арсланов on 27.10.2024.
+//
+
+import Foundation
+
+protocol UserProfileServiceProtocol {
+    func fetchUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void)
+}
+
+final class UserProfileService: UserProfileServiceProtocol {
+    private let networkClient: NetworkClient
+    
+    init(networkClient: NetworkClient) {
+        self.networkClient = networkClient
+    }
+    
+    func fetchUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
+        let request = UserProfileRequest()
+        networkClient.send(request: request) { [weak self] result in
+            guard self != nil  else { return }
+            switch result {
+            case .success(let data):
+                do {
+                    let profile = try JSONDecoder().decode(UserProfile.self, from: data)
+                    completion(.success(profile))
+                } catch {
+                    completion(.failure(NetworkClientError.parsingError))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
