30a15eb8a7b0da3c5c0aa5f2067ba4c465a5d14c
Morozov Artem <Vjhjpjaa32@yandex.ru>
2024-10-30 20:34:57 +0300

    Добавил CollectionCellViewModel

---
+++ FakeNFT/Models/CollectionCellViewModel.swift
@@ -0,0 +1,12 @@
+//  Created by Artem Morozov on 30.10.2024.
+
+import Foundation
+
+struct CollectionCellViewModel {
+    let name: String
+    let image: URL?
+    let rating: Int
+    let price: Double
+    let isAddedToCart: Bool
+    let isFavorited: Bool
+}
