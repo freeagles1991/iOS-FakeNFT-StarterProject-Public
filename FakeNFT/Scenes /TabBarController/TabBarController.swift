3abc57d952a84d5ccf1189e7bdc8211a99f2d583
Arthur <Fokusnik90999@gmail.com>
2024-10-31 12:50:53 +0500

    Настроил TabBar

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -1,45 +1,65 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-
-
     var servicesAssembly: ServicesAssembly!
-
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(named: "catalogTabBarIcon_nonactive"),
-        selectedImage: UIImage(named: "catalogTabBarIcon_active")
-    )
-    
-    private let cartTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.cart", comment: ""),
-        image: UIImage(named: "cartTabBarIcon_nonactive"),
-        selectedImage: UIImage(named: "cartTabBarIcon_active")
-    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let catalogAssembly = CatalogAssembly(servicesAssembly: servicesAssembly)
-        let cataloViewController = catalogAssembly.build()
-        let catalogNavigationController = UINavigationController(rootViewController: cataloViewController)
-        
-        catalogNavigationController.tabBarItem = catalogTabBarItem
-        
-        let cartNavigationController = configureCartVC()
-
-        viewControllers = [catalogNavigationController, cartNavigationController]
+        setupTabBar()
         view.backgroundColor = .systemBackground
     }
     
-    //Конфигурируем вкладку корзины
-    private func configureCartVC() -> UIViewController {
+    private func setupTabBar() {
+        let catalogNavigationController = configureCatalogVC()
+        let cartNavigationController = configureCartVC()
+        let profileController = createProfileModule()
+        
+        viewControllers = [profileController, catalogNavigationController, cartNavigationController]
+    }
+    
+    private func configureCatalogVC() -> UINavigationController {
+        let catalogAssembly = CatalogAssembly(servicesAssembly: servicesAssembly)
+        let catalogViewController = catalogAssembly.build()
+        let catalogNavigationController = UINavigationController(rootViewController: catalogViewController)
+        catalogNavigationController.tabBarItem = createTabBarItem(
+            title: NSLocalizedString("Tab.catalog", comment: ""),
+            imageName: "catalogTabBarIcon_nonactive",
+            selectedImageName: "catalogTabBarIcon_active"
+        )
+        return catalogNavigationController
+    }
+    
+    private func configureCartVC() -> UINavigationController {
         let cartAssembly = CartAssembly(servicesAssembler: servicesAssembly)
         let cartController = cartAssembly.build()
         let cartNavigationController = UINavigationController(rootViewController: cartController)
         cartNavigationController.modalPresentationStyle = .fullScreen
-        cartNavigationController.tabBarItem = cartTabBarItem
+        cartNavigationController.tabBarItem = createTabBarItem(
+            title: NSLocalizedString("Tab.cart", comment: ""),
+            imageName: "cartTabBarIcon_nonactive",
+            selectedImageName: "cartTabBarIcon_active"
+        )
         return cartNavigationController
     }
     
+    private func createProfileModule() -> UIViewController {
+        let profileBuilder = AssemblyBuilderProfile(servicesAssembly: servicesAssembly)
+        let profileController = profileBuilder.createProfileModule()
+        profileController.tabBarItem = createTabBarItem(
+            title: NSLocalizedString("Tab.profile", comment: ""),
+            imageName: "profileTabBarIcon_nonactive",
+            selectedImageName: "profileTabBarIcon_active"
+        )
+        return profileController
+    }
+    
+    //MARK: - Create Tab Bar Item
+    private func createTabBarItem(title: String, imageName: String, selectedImageName: String) -> UITabBarItem {
+        return UITabBarItem(
+            title: title,
+            image: UIImage(named: imageName),
+            selectedImage: UIImage(named: selectedImageName)
+        )
+    }
 }
